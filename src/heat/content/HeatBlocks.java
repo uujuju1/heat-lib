ca4ac60d3879a2ca388b8f55a5be60466b265260
uujuju <84468589+uujuju1@users.noreply.github.com>
2022-02-10 17:01:15 -0300

    Add files via upload

---
+++ src/heat/content/HeatBlocks.java
@@ -0,0 +1,19 @@
+package heat.content;
+
+import mindustry.ctype.ContentList;
+import mindustry.world.Block;
+import heat.world.heat.distribution.*;
+
+public class HeatBlocks implements ContentList {
+	public static Block heatConveyor;
+
+	@Override
+	public void load() {
+		heatConveyor = new HeatConveyor("heat-conveyor") {{
+			size = 1;
+			health = 50;
+			minHeat = 25;
+			maxHeat = 1000;
+		}};
+	}
+}
