3178dd90a1c6626755a8eec3b188981ef0d4ce3c
uujuju <84468589+uujuju1@users.noreply.github.com>
2022-02-10 15:51:20 -0300

    Add files via upload

---
+++ src/heat/world/heat/distribution/HeatConveyor.java
@@ -0,0 +1,31 @@
+package heat.world.heat.distribution;
+
+import heat.world.heat.*;
+
+public class HeatConveyor extends HeatBlock {
+	public TextureRegion regions, baseRegion, heatRegion;
+
+	public HeatConveyor(String name) {
+		super(name);
+		rotate = true;
+	}
+
+	@Override
+	public void load() {
+		super.load();
+		Core.atlas.find(name + "-states");
+	}
+
+	public class HeatConveyorBuild extends HeatBlock.HeatBlockBuild {
+		@Override
+		public void draw() {
+			Draw.rect(baseRegion, x, y, 0);
+			// Draw.color(heatColor);
+			Draw.alpha(heatf());
+			Draw.rect(heatRegion, x, y, 0);
+			for (int i = 0; i < 10; i++) {
+				Draw.rect(TextureManager.getRegions(regions, 5, 2)[i], x - 16 + (i*8), y - 4 + (i>5 ? 0 : 8), this.rotate ? this.rotdeg(), 0);
+			}
+		}
+	}
+}
